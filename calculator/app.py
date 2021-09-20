003834287f5d0734cd105827cd99fedabd6b28d1
kishor <kishorbrindha18@gmail.com>
2021-09-20 11:44:31 +0530

    some changes in app.py

--- calculator/app.py
+++ calculator/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+rebdgtbdh
+ebgbgbhgb
+enhhnnapp = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST']) # To render Homepage
+@oute('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template('index.html')
 
